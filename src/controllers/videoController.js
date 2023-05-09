a212e3eba732d9d6245d03e74dc38e5b5901a9f3
wannywan <choijw1116@gmail.com>
2023-05-09 20:06:10 +0900

    Add pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,14 @@
 export const trending = (req, res) => {
-  res.send("Homepage videos ");
+  //   res.send("Homepage videos ");
+  res.render("home", { pageTitle: "Home" }); //pug로 사용하는 방법. 첫번째 인자로 보여줄 view의 이름을 적는다. 우리의 경우 home화면이므로 home을 적어준다. 그러면 home.pug 파일을 렌더링 해줄 수 있다.
 };
 export const watch = (req, res) => {
   //   console.log(req.params);
-  res.send(`watch video #${req.params.id}`);
+  //   res.send(`watch video #${req.params.id}`);
+  res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => {
-  res.send("Edit videos ");
+  res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => {
   res.send("Search");
--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 import express from "express";
-import morgan from "morgan";
+import morgan from "morgan"; //morgan함수는 middleware를 return 해준다 morgan함수는 좀 더 정교하게 정보를 보여준다
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
+// console.log(process.cwd());
+
 const app = express(); //application 생성
 const logger = morgan("dev");
-//global middlware 만들기
-app.use(logger);
-//morgan함수는 middleware를 return 해준다
-//morgan함수는 좀 더 정교하게 정보를 보여준다
 
+//Express에게 html헬퍼로 pug를 쓰겠다고 말해줘야함
+//view engine을 pug로 설정하면 Express가 html을 리턴하기 위해서 pug를 사용한다.
+
+//view engine을 pug로 세팅하기
+app.set("view engine", "pug");
+//cwd 경로 변경해주기
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 //router사용방법
 app.use("/", globalRouter); //url을 첫번쨰인자, router를 두번쨰인자
 app.use("/users", userRouter);
